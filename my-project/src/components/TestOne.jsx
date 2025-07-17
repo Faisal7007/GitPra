454939f01175cbad63fa574a7bf83545cb8d3bc2
FaisalAnsari <faisalansari30121998@gmail.com>
2025-07-17 22:43:32 +0530

    designation added

--- my-project/src/components/TestOne.jsx
+++ my-project/src/components/TestOne.jsx
@@ -4,6 +4,7 @@ const TestOne = () => {
   return (
     <div className='text-red-500 '>
     <h2>Hi this is Arshad</h2>
+    <h3>React Developer</h3>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque sit, animi exercitationem aspernatur porro impedit optio officiis veniam adipisci esse!</p>
     </div>
   )
