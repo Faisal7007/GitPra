0514e6bf6843e2315d131b80f5b39303cc1e18a4
FaisalAnsari <faisalansari30121998@gmail.com>
2025-07-17 22:40:07 +0530

    test two changes

--- my-project/src/components/TestOne.jsx
+++ my-project/src/components/TestOne.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TestOne = () => {
   return (
     <div className='text-red-500 '>
-    <h2>Hi this is Faisal</h2>
+    <h2>Hi this is Arshad</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque sit, animi exercitationem aspernatur porro impedit optio officiis veniam adipisci esse!</p>
     </div>
   )
